d5f65b22a9736b4e8471b324c52de1fb3ea6bf60
facebook.me.secure <facebook.me.secure@gmail.com>
2025-10-28 07:43:33 +0000

    nice

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,7 +2,9 @@
   channel = "stable-24.05";
   packages = [
     pkgs.nodejs_20
+    pkgs.docker-compose
   ];
+  services.docker.enable = true;
   idx.extensions = [
     "svelte.svelte-vscode"
     "vue.volar"
